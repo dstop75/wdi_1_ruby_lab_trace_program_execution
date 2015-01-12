098ce18ca373f7b823239edd736157f74ff0a720
Dan Stoppelman <dstop75@gmail.com>
2015-01-12 14:55:46 -0500

    Add comments to triangle sources.
    Add comment about loading code using relative path.

--- lib/triangle/valid_triangle.rb
+++ lib/triangle/valid_triangle.rb
@@ -1,5 +1,8 @@
-# Returns true/false if a triangle is valid
+# Returns true if a triangle is valid,
+# => and false if the triangle is invalid.
 # as per the Triangle Inequality Theorem
+# Note: any 1 side must be shorter than the sum
+# => of the other 2 sides.
 def valid_triangle?(side_a, side_b, side_c)
   (side_a + side_b) > side_c &&
   (side_b + side_c) > side_a &&
