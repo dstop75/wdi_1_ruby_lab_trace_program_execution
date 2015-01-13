6b1728a7ee66851aefc8d468bd12d8088e512302
Dan Stoppelman <dstop75@gmail.com>
2015-01-13 00:48:10 -0500

    rename triangles.rb as triangle.rb add/edit code for is_prime add comments for triangle.rb and valid_triangle.rb

--- lib/is_prime.rb
+++ lib/is_prime.rb
@@ -1,2 +1,38 @@
+#  convoluted but somewhat optimized version
 def is_prime?(x)
+  if x < 2
+    false
+  elsif x == 2
+    true
+  elsif x % 2 == 0
+    false
+  else
+    i = 3
+    while i < x
+      if x % i == 0
+        return false
+      else
+        x /= i
+        i += 2
+      end
+    end
+    true
+  end
 end
+
+#  simpler version
+=begin
+def is_prime?(x)
+  if x < 2
+    return false
+  count = 2
+  while count < x
+    if x % count == 0
+      return false
+    else
+      count += 1
+    end
+  end
+  true
+end
+=end
--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,1 +1,3 @@
+#  Tell ruby to load the code that is in the file
+#  ./triangle/valid_triangle.rb
 require_relative 'triangle/valid_triangle'
